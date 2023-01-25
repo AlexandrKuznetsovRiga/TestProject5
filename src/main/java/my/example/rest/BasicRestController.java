98e40030784151ae2c6ec11ac1de837ca7441f42
Aleksandr Kuznetsov <aleksandr.kuznetsov@corp.mail.ru>
2023-01-25 08:38:06 +0200

    code clean

--- src/main/java/my/example/rest/BasicRestController.java
+++ src/main/java/my/example/rest/BasicRestController.java
@@ -3,13 +3,10 @@ package my.example.rest;
 import my.example.service.CountryGraphService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -25,19 +22,14 @@ public class BasicRestController {
 
 
     @RequestMapping(path = "/routing/{origin}/{destination}")
-    public RoutingResponse getRouting(@PathVariable String origin, @PathVariable String destination, Model model) {
+    public RoutingResponse getRouting(@PathVariable String origin, @PathVariable String destination) {
         List<String> cCodes = null;
         try {
             cCodes = graphService.findRoute(origin, destination);
         } catch (IllegalArgumentException e) {
-
-            model.addAttribute("ERROR_MESSAGE", "Invalid input!");
-            model.addAttribute("ERROR_MESSAGE_en_US", "Invalid input!");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
         if (cCodes == null || cCodes.isEmpty()) {
-            model.addAttribute("ERROR_MESSAGE", "There is no land crossing!");
-            model.addAttribute("ERROR_MESSAGE_en_US", "There is no land crossing!");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There is no land crossing!");
 
         }
